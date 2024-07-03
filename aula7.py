3aca6f37b6ac2fc841f53aa30c245578c88b67c6
unochapeco <unochapeco@unochapeco.hro.local>
2024-07-03 17:05:49 -0300

    First commit

---
+++ aula7.py
@@ -0,0 +1,21 @@
+# sep=' ', para adicionar algo entre os valores, e end=' ' para adicionar valor no final. 
+print(21, 54, sep=',', end='\n')
+
+print("Luiz \"Otavio\" ")
+
+# Tipos de variavel
+print(type("Otavio"))
+print(type(5))
+print(type(5), type("Teste"), type(-3))
+
+# Boolean 
+print(10 == 10)
+
+
+# Convertendo valores
+print(int('1'), type(int('1')))
+
+
+# Variaveis
+nome_completo = "Jacinto Perreira"
+print(nome_completo)
