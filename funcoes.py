162cbe4cd52f12a97c2a0da3913ee95dd606f333
unochapeco <unochapeco@unochapeco.hro.local>
2024-07-10 17:39:00 -0300

    funções

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,15 @@
-def imprimir(a, c):
-    print(a + c)
+# def imprimir(x, y):
+#     print(f'{x=} {y=}', '|', 'x + y = ', x + y)
+
+# imprimir(5, 7)
 
 
+def soma(x, y):
+    print(x + y)
 
-imprimir(1, 3)
+
+soma(1, 2)
+soma(3, 5)
+soma(100, 200)
+
+#Teste ok
